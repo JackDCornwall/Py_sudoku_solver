796436a86d1ea234144c9689ea2f341639affcfb
JackDCornwall <contact@jackcornwall.co.uk>
2021-06-01 10:15:10 +0100

    Initial commit

---
+++ Sudoku_solver_010621.py
@@ -0,0 +1,10 @@
+'''
+#Created by: Jack Cornwall
+#Email: jackcornwall91@gmail.com
+#Git hub: https://github.com/JackDCornwall
+#Website:https://jackcornwall.co.uk/
+
+#Project: Sudoku solver
+#Date: 010621
+#Description: Point your camera at a sudoku, centre it in the square and boom it will be solved for you.
+'''
